a0a96f310a5e3d47e9026c8eca4dcb1ea65a363a
Leonardo Lamoia Rosa <leonardotcinf@gmail.com>
2024-01-24 21:16:01 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Leonardo Lamoia on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
